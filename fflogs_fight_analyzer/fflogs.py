12bd1562595cfa690727d6e6af276a8b832d52ad
Panic Stevenson <panic.stevenson@gmail.com>
2020-02-02 13:07:52 -0800

    Add Python Type Hinting
    Add type hints to functions to encourage use of static analysis tools
    capable of finding issues in the code before they arise.

--- fflogs_fight_analyzer/fflogs.py
+++ fflogs_fight_analyzer/fflogs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from typing import Optional
 import logging
 import os
 import sys
@@ -10,7 +11,7 @@ FF_LOGS_API_URL = FF_LOGS_BASE_URL + "v1/"
 FF_LOGS_CLASSES_URL = FF_LOGS_API_URL + "classes"
 
 
-def _get_api_key():
+def _get_api_key() -> str:
     api_key = os.getenv("FF_LOGS_API_KEY")
     if not api_key:
         api_key = input("Enter your FF Logs API public key (https://www.fflogs.com/profile): ")
@@ -20,7 +21,7 @@ def _get_api_key():
 class Client:
     logger = logging.getLogger(__name__)
 
-    def __init__(self, api_key=None):
+    def __init__(self, api_key: str = None):
         self.logger.debug("Attempting to connect to FF Logs...")
         if not self._establish_connection():
             self.logger.error("Unable to establish connection with FF Logs... Exiting.")
@@ -37,7 +38,7 @@ class Client:
         self.logger.debug("Successfully validated FF Logs API key.")
         self.api_key = api_key
 
-    def _get(self, url, params=None):
+    def _get(self, url: str, params: dict = None) -> Optional[requests.Response]:
         try:
             response = requests.get(url, params)
             response.raise_for_status()
@@ -48,14 +49,15 @@ class Client:
             self.logger.error("HTTP request timeout.", exc_info=True)
         except requests.exceptions.RequestException:
             self.logger.error("A fatal error has occurred.", exc_info=True)
+        return None
 
-    def _establish_connection(self):
+    def _establish_connection(self) -> Optional[requests.Response]:
         return self._get(FF_LOGS_BASE_URL)
 
-    def _validate_api_key(self, api_key):
+    def _validate_api_key(self, api_key: str) -> Optional[requests.Response]:
         return self._get(FF_LOGS_CLASSES_URL, {"api_key": api_key})
 
-    def get(self, api_endpoint, parameters=None):
+    def get(self, api_endpoint: str, parameters: dict = None) -> Optional[requests.Response]:
         request_url = FF_LOGS_API_URL + api_endpoint
         if not parameters:
             parameters = {}
