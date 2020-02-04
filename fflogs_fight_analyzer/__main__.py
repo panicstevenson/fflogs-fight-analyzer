3ee66463ac06510dfe92c8a5cdf67e2b24128226
Panic Stevenson <panic.stevenson@gmail.com>
2020-02-03 20:34:48 -0800

    Add ArgumentParser
    Add ArgumentParser to the module, enabling users to pass in parameters
    and options via the commandline. Initial parameter is the FF Logs web
    API public key.

--- fflogs_fight_analyzer/__main__.py
+++ fflogs_fight_analyzer/__main__.py
@@ -1,14 +1,27 @@
+import argparse
 import logging
 import sys
 from fflogs_fight_analyzer import fflogs
 
 
-def main():
-    client = fflogs.Client()
+def _create_argument_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="fflogs_fight_analyzer",
+        description="Simple interface to parse elements and events from FF Logs' web API.",
+    )
+    parser.add_argument(
+        "--api-key",
+        type=str,
+        help="FF Logs public API key, which can be found at https://www.fflogs.com/profile",
+    )
+    return parser
+
+
+def _main(args: argparse.Namespace):
+    client = fflogs.Client(args.api_key)
     client.get("zones")
 
 
 if __name__ == "__main__":
-    LOGGER = logging.getLogger(__name__)
     logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
-    sys.exit(main())
+    sys.exit(_main(_create_argument_parser().parse_args()))
